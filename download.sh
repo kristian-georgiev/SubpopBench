01846555d94cdda492a9059f4f7c2ae9918825ca
Kristian Georgiev <krisgrg@mit.edu>
2024-04-06 13:54:10 -0400

    ez

---
+++ download.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+python -m subpopbench.scripts.download --data_path /mnt/xfs/projects/trak_fairness/after_refactor/DATA --download celeba
+
+python -m subpopbench.scripts.download --data_path /mnt/xfs/projects/trak_fairness/after_refactor/DATA --download imagenetbg
+
+python -m subpopbench.scripts.download --data_path /mnt/xfs/projects/trak_fairness/after_refactor/DATA --download civilcomments
+
+python -m subpopbench.scripts.download --data_path /mnt/xfs/projects/trak_fairness/after_refactor/DATA --download multinli
+
+python -m subpopbench.scripts.download --data_path /mnt/xfs/projects/trak_fairness/after_refactor/DATA --download metashift
+
+python -m subpopbench.scripts.download --data_path /mnt/xfs/projects/trak_fairness/after_refactor/DATA --download nico++
+
+python -m subpopbench.scripts.download --data_path /mnt/xfs/projects/trak_fairness/after_refactor/DATA --download cmnist
+
+python -m subpopbench.scripts.download --data_path /mnt/xfs/projects/trak_fairness/after_refactor/DATA --download breeds
+
+python -m subpopbench.scripts.download --data_path /mnt/xfs/projects/trak_fairness/after_refactor/DATA --download celeba
